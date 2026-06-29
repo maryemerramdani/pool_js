466b25bc7dcd49351bc31471edd657a390f97a7d
merramda <maryemerramdani52@gmail.com>
2026-06-29 18:06:14 +0100

    first

--- returns.js
+++ returns.js
@@ -1,11 +1,21 @@
-function id(Value)
+function id(arg)
 {
-    return Value;
+    return arg;
 }
-function getLength(input)
+function getLength(arg)
 {
-    if(typeof input==='string'||Array.isArray(INPUT))
-{
-    return input.length;
+    if(typeof(arg)=="object"||typeof(arg)=="string"){
+        return arg.length;
+    }
+    return;
 }
-}
+// Testing id function
+console.log(id(5)); // Should output 5
+console.log(id("hello")); // Should output 'hello'
+console.log(id([1, 2, 3])); // Should output [1, 2, 3]
+
+// Testing getLength function
+console.log(getLength("hello")); // Should output 5
+console.log(getLength([1, 2, 3])); // Should output 3
+console.log(getLength([])); // Should output 0
+console.log(getLength("")); // Should output 0
