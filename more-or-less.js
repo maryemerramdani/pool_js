871bcd941d3b4d2abbcd26c125a1de4b814e480a
merramda <maryemerramdani52@gmail.com>
2026-06-29 17:28:02 +0100

    mutability

---
+++ more-or-less.js
@@ -0,0 +1,19 @@
+function more(Value) {
+    return Value+1;
+}
+
+function less(Value) {
+    return Value-1;
+}
+
+function add(a,b) {
+    return a+b;
+}
+
+function sub(a,b) {
+    return a-b;
+}
+console.log(more(5)); // Should output 6
+console.log(less(5)); // Should output 4
+console.log(add(5, 3)); // Should output 8
+console.log(sub(5, 3)); // Should output 2
