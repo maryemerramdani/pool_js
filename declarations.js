37c931ed0bb4581a449cef9c98b5fc4b59d29fac
merramda <maryemerramdani52@gmail.com>
2026-06-29 16:16:28 +0100

    selected

--- declarations.js
+++ declarations.js
@@ -2,14 +2,14 @@ const escapeStr = "\`,\/,\/,\",\'";
 const arr=Object.freeze([4,"2"]);
 const obj =Object.freeze(
 {
-    "str" : "zone01",
-    "num" : 44,
-    "bool" : true,
-    "undef" : undefined
+    str : "zone01",
+    num : 44,
+    bool : true,
+    undef : undefined
 });
-const nested=Object.freeze(
-{
-    arr:Object.freeze([4,undefined,'2']),
+
+const nested=Object.freeze({
+    arr:Object.freeze([4,undefined,"2"]),
     obj:Object.freeze({
         "str": "bonjour",
         "num": 5,
