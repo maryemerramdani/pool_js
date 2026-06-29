249875060184a5bc39a4f43c2e3e4f19d010fd6c
merramda <maryemerramdani52@gmail.com>
2026-06-29 16:59:15 +0100

    mutability

---
+++ declarations.js
@@ -0,0 +1,19 @@
+const escapeStr = "`\\\/\"'";
+const arr=Object.freeze([4,"2"]);
+const obj =Object.freeze(
+{
+    str : "zone01",
+    num : 44,
+    bool : true,
+    undef : undefined
+});
+
+const nested=Object.freeze({
+    arr:Object.freeze([4,undefined,"2"]),
+    obj:Object.freeze({
+        str: "bonjour",
+        num: 5,
+        bool:true
+    })
+});
+
--- mutability.js
+++ mutability.js
@@ -3,19 +3,19 @@ const person = {
   age: 77,
   country: "US",
 };
-const colone1 = structuredClone(person);
-const colone2 = structuredClone(person);
+const clone1 = structuredClone(person);
+const clone2 = structuredClone(person);
 const samePerson =person;
 person.age+=1;
 person.country='FR';
 console.log(person.age === 78);
 console.log(person.country === "FR");
 
-console.log(colone1.age === 77);
-console.log(colone1.country === "US");
+console.log(clone1.age === 77);
+console.log(clone1.country === "US");
 
-console.log(colone2.age === 77);
-console.log(colone2.country === "US");
+console.log(clone2.age === 77);
+console.log(clone2.country === "US");
 
 console.log(samePerson.age ===78);
 console.log(samePerson.country === "FR");
