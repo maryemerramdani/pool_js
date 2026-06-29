a40f60f980abd6738c1cedaccae1373ab01b547f
merramda <maryemerramdani52@gmail.com>
2026-06-29 17:13:50 +0100

    mutability

--- mutability.js
+++ mutability.js
@@ -3,14 +3,3 @@ const clone2 = structuredClone(person);
 const samePerson =person;
 person.age+=1;
 person.country='FR';
-console.log(person.age === 78);
-console.log(person.country === "FR");
-
-console.log(clone1.age === 77);
-console.log(clone1.country === "US");
-
-console.log(clone2.age === 77);
-console.log(clone2.country === "US");
-
-console.log(samePerson.age === 78);
-console.log(samePerson.country === "FR");
