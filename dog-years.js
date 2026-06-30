4ac8c150bce1dc5368fe2004f5fae4e71b2493f7
merramda <maryemerramdani52@gmail.com>
2026-06-30 14:11:18 +0100

    dogyears

--- dog-years.js
+++ dog-years.js
@@ -1,21 +1,17 @@
-
-function dogYears(planet,seconds)
-{
-    const earthYearInSeconds = 31557600; 
-    const dogYearsFactor = 7;
-    const orbitalperiod ={
-        terre : 1.0,
-        Mercure: 0.2408467,
-        venus: 0.6151926,
-        mars: 1.8808158,
-        jupiter: 11.862615,
-        saturne: 29.447498,
-        uranus: 84.016846,
-        neptune: 164.79132,
-    };
-    const ageInPlanetYears =
-        seconds / (earthYearInSeconds * orbitalperiod[planet]);
-    const ageInDogYears = ageInPlanetYears * dogYearsFactor;
-    return Number(ageInDogYears.toFixed(2));
-};
-
+function dogYears(planet, seconds) {
+  const earthYearInSeconds = 31557600;
+  const dogYearsFactor = 7;
+  const orbitalPeriod = {
+    mercury: 0.2408467,
+    venus: 0.6151926,
+    earth: 1.0,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132,
+  };
+  const ageInPlanetYears = seconds / (earthYearInSeconds * orbitalPeriod[planet]);
+  const ageInDogYears = ageInPlanetYears * dogYearsFactor;
+  return Number(ageInDogYears.toFixed(2));
+}
