c2eff781df6fa7288b9c9cc89cc9f39685d64a95
merramda <maryemerramdani52@gmail.com>
2026-06-30 16:04:33 +0100

    methodman

--- method-man.js
+++ method-man.js
@@ -1,21 +1,24 @@
-function words(str) {
-  return str.split(' ');
+function words(str) 
+{
+  return str.split("");
 }
 
-function sentence(arr) {
-  return arr.join(' ');
+function sentence(arr) 
+{
+  return arr.join("");
 }
 
-function yell(str) {
+function yell(str) 
+{
   return str.toUpperCase();
 }
 
 function whisper(str) {
-  return '*' + str.toLowerCase() + '*';
+  return '*' + str.toLowerCase() + "*";
 }
 
 function capitalize(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  return str[0].toUpperCase()+str.slice(1).tolowerCase();
 }
 // Test words function
 console.log(words("Hello there, how are you?"));
