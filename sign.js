335631f19a263135f2c5620ff3d8474a8e22e150
merramda <maryemerramdani52@gmail.com>
2026-06-30 17:21:45 +0100

    sign

---
+++ sign.js
@@ -0,0 +1,13 @@
+function sign (a){
+    if(a>0){
+        return 1;
+    }
+    if(a<0){
+        return -1;
+    }
+        return 0;
+}
+function samesign(a,b){
+    return sign(a) == sign(b);
+
+}
