25442fb4cb6c5679e236a4ed761d48ca4137187b
merramda <maryemerramdani52@gmail.com>
2026-06-30 16:17:43 +0100

    methodman

--- method-man.js
+++ method-man.js
@@ -20,22 +20,3 @@ function whisper(str) {
 function capitalize(str) {
   return str[0].toUpperCase()+str.slice(1).tolowerCase();
 }
-// Test words function
-console.log(words("Hello there, how are you?"));
-// Should output: ["Hello", "there,", "how", "are", "you?"]
-
-// Test sentence function
-console.log(sentence(["This", "is", "a", "test"]));
-// Should output: "This is a test"
-
-// Test yell function
-console.log(yell("be quiet"));
-// Should output: "BE QUIET"
-
-// Test whisper function
-console.log(whisper("EXCUSE ME"));
-// Should output: "*excuse me*"
-
-// Test capitalize function
-console.log(capitalize("rUNNING"));
-// Should output: "Running"
