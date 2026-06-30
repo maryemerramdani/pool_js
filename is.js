ca073b552aac4238b5f2504aa34301c21ea12671
merramda <maryemerramdani52@gmail.com>
2026-06-30 17:48:20 +0100

    is

---
+++ is.js
@@ -0,0 +1,16 @@
+const is = {};
+
+is.num = (value) => typeof value === "number";
+is.nan = (value) => Number.isNaN(VALUE);
+is.str = (value) => typeof value === "string";
+is.bool = (value) => typeof value === "boolean";
+is.undef = (value) =>value === undefined;
+is.def = (value) =>value !== undefined;
+is.arr =(value) =>Array.isArray(value);
+is.obj = (value) =>
+    value !== null &&
+    typeof value === "object" &&
+    !Array.isArray(value);
+is.fun = (value) =>typeof value === "function"
+is.truthy = (value) => (value) =>!! value;
+is.falsy = (value) => (value) => !value;
