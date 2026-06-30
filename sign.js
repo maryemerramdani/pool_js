cece4ef18fc0d4a1b22cbef149311a6ca98f086e
merramda <maryemerramdani52@gmail.com>
2026-06-30 17:24:12 +0100

    sign

--- sign.js
+++ sign.js
@@ -7,7 +7,18 @@ function sign (a){
     }
         return 0;
 }
-function samesign(a,b){
+function sameign(a,b){
     return sign(a) == sign(b);
-
 }
+// Test sign function
+console.log(sign(3)); // Should output 1
+console.log(sign(-3)); // Should output -1
+console.log(sign(0)); // Should output 0
+
+// Test sameSign function
+console.log(sameSign(3, 4)); // Should output true
+console.log(sameSign(-3, -4)); // Should output true
+console.log(sameSign(0, 0)); // Should output true
+console.log(sameSign(3, -4)); // Should output false
+console.log(sameSign(0, 4)); // Should output false
+console.log(sameSign(0, -4)); // Should output false
