6816d1ea6bde3c094e1720e0da51e690e9515a61
merramda <maryemerramdani52@gmail.com>
2026-06-30 11:26:03 +0100

    change

--- change.js
+++ change.js
@@ -11,19 +11,5 @@ function get(key){
 
 function set(key,value){
     sourceObject[key]=value;
-    return;
+    return value ;
 }
-// Test get function
-console.log(get("num")); // Should output 42
-console.log(get("bool")); // Should output true
-console.log(get("str")); // Should output 'some text'
-
-// Test set function
-console.log(set("num", 77)); // Should output 77
-console.log(set("bool", false)); // Should output false
-console.log(set("str", "new text")); // Should output 'new text'
-
-// Verify the changes
-console.log(get("num")); // Should output 77
-console.log(get("bool")); // Should output false
-console.log(get("str")); // Should output 'new text'
