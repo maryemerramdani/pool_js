2ee04f98fb2e87c179caf388633e45062d537796
merramda <maryemerramdani52@gmail.com>
2026-06-30 11:32:37 +0100

    change

--- change.js
+++ change.js
@@ -1,8 +1,15 @@
-function get(key){
+const sourceObject = {
+  num: 42,
+  bool: true,
+  str: "some text",
+  log: console.log,
+};
+
+function Get(key){
     return sourceObject[key];
 }
 
-function set(key, value){
-    sourceObject[key] = value;
-    return  sourceObject[key];
+function Set(key,value){
+    sourceObject[key]=value;
+    return sourceObject[key];
 }
