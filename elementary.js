2689f49aafed39356dd173b903066ecfb7e04350
merramda <maryemerramdani52@gmail.com>
2026-07-01 15:59:06 +0100

    elementary

--- elementary.js
+++ elementary.js
@@ -1,28 +1,31 @@
-function multiply (a,b){
-    let result = 0;
-    let isNegative = ((a<0 )!== (b<0));
-    let absA = a < 0 ? -a : a;
-    let absB = b < 0 ? -b : b;
-for(i=0 ; i<absB ; i ++ ){
-    result = result+absA;
-}
-return isNegative ? -result : result ;
+function multiply(a, b) {
+  let result = 0;
+  let isNegative = (a < 0) !== (b < 0);
+  let absA = a < 0 ? -a : a;
+  let absB = b < 0 ? -b : b;
+
+  for (let i = 0; i < absB; i++) {
+    result += absA;
+  }
+  return isNegative ? -result : result;
 }
 
-function divide(a,b){
-if (b === 0) return NaN;
-    let result = 0;
-    let isNegative = ((a<0) !== (b<0));
-    let absA = a < 0 ?  -a : a;
-    let absB = b < 0 ?  -b : b ;
-while(absA >= absB){
+function divide(a, b) {
+  if (b === 0) return NaN;
+  let result = 0;
+  let isNegative = (a < 0) !== (b < 0);
+  let absA = a < 0 ? -a : a;
+  let absB = b < 0 ? -b : b;
+
+  while (absA >= absB) {
     absA -= absB;
     result++;
+  }
+  return isNegative ? -result : result;
 }
-return isNegative ? -result : result;
-}
-function modulo (a,b){
-   if (b === 0) return NaN;
+
+function modulo(a, b) {
+  if (b === 0) return NaN;
   let absA = a < 0 ? -a : a;
   let absB = b < 0 ? -b : b;
 
@@ -30,4 +33,4 @@ function modulo (a,b){
     absA -= absB;
   }
   return a < 0 ? -absA : absA;
-} 
+}
