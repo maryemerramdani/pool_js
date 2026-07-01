929da9bd149881608a10870eccf658bddac393cb
merramda <maryemerramdani52@gmail.com>
2026-07-01 16:21:37 +0100

    collections

--- collections.js
+++ collections.js
@@ -1,62 +1,47 @@
-function arrToSet(arr) {
+export function arrToSet(arr) {
   return new Set(arr);
 }
 
-function arrToStr(arr) {
+export function arrToStr(arr) {
   return arr.join('');
 }
 
-function setToArr(set) {
+export function setToArr(set) {
   return Array.from(set);
 }
 
-function setToStr(set) {
+export function setToStr(set) {
   return Array.from(set).join('');
 }
 
-function strToArr(str) {
+export function strToArr(str) {
   return str.split('');
 }
 
-function strToSet(str) {
+export function strToSet(str) {
   return new Set(str);
 }
 
-function mapToObj(map) {
+export function mapToObj(map) {
   return Object.fromEntries(map);
 }
 
-function objToArr(obj) {
+export function objToArr(obj) {
   return Object.values(obj);
 }
 
-function objToMap(obj) {
+export function objToMap(obj) {
   return new Map(Object.entries(obj));
 }
 
-function arrToObj(arr) {
+export function arrToObj(arr) {
   return { ...arr };
 }
 
-function strToObj(str) {
+export function strToObj(str) {
   return { ...str.split('') };
 }
 
-function superTypeOf(value) {
+export function superTypeOf(value) {
   return Object.prototype.toString.call(value).slice(8, -1);
 }
-
-module.exports = {
-  arrToSet,
-  arrToStr,
-  setToArr,
-  setToStr,
-  strToArr,
-  strToSet,
-  mapToObj,
-  objToArr,
-  objToMap,
-  arrToObj,
-  strToObj,
-  superTypeOf,
-};
