b7954a9f44ed636a2d04e1c27f426f8f6c452d1a
merramda <maryemerramdani52@gmail.com>
2026-07-02 17:30:21 +0100

    chunky

--- chunky.js
+++ chunky.js
@@ -1,44 +1,15 @@
-function split(str, separator) {
+function chunk(arr, size) {
   const result = [];
-  if (separator === "") {
-    for (let i = 0; i < str.length; i++) {
-      result[result.length] = str[i];
+
+  for (let i = 0; i < arr.length; i += size) {
+    const subArray = [];
+
+    for (let j = i; j < i + size && j < arr.length; j++) {
+      subArray[subArray.length] = arr[j];
     }
-    return result;
+
+    result[result.length] = subArray;
   }
 
-  let current = "";
-
-  for (let i = 0; i < str.length; i++) {
-    let match = true;
-
-    for (let j = 0; j < separator.length; j++) {
-      if (str[i + j] !== separator[j]) {
-        match = false;
-        break;
-      }
-    }
-    if (match) {
-      result[result.length] = current;
-      current = "";
-      i += separator.length - 1;
-    } else {
-      current += str[i];
-    }
-  }
-
-  result[result.length] = current;
-  return result;
-}
-
-function join(arr, separator) {
-  let result = "";
-
-  for (let i = 0; i < arr.length; i++) {
-    if (i > 0) {
-      result += separator;
-    }
-    result += arr[i];
-  }
   return result;
 }
