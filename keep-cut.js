f185fb9ea496e145638706453783cce808365368
merramda <maryemerramdani52@gmail.com>
2026-07-02 13:09:21 +0100

    keepcut"
    "
    "keep-cut"
    "keepcut"
    git commit -m"keepcut"
    "
    é

---
+++ keep-cut.js
@@ -0,0 +1,23 @@
+function cutFirst (str){
+    return str.slice (2);
+}
+
+function cutLast (str){
+    return str.slice(0,-2);
+}
+
+function cutFirstLast(str){
+    return str.slice(2,-2);
+}
+
+function keepFirst(str){
+    return str.slice(0,2);
+}
+
+function keeplast (str){
+    return str.slice(0,-2);
+}
+
+function keepFirsLast (str){
+    return str.slice(2,-2);
+}
