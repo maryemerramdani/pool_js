349ed5693dcc853dca5dfbc9b6e0a0302047a4f6
merramda <maryemerramdani52@gmail.com>
2026-07-02 11:42:44 +0100

    indexof

--- index-of.js
+++ index-of.js
@@ -1,6 +1,5 @@
-function indexOf ( arr , value , start=0){
+function indexOf (arr, value, start=0){
     if(!Array.isArray(arr)) return -1;
-
     for(let i = start ; i < arr.length ; i++){
         if(arr[i] === value){
             return i ;
@@ -8,10 +7,10 @@ function indexOf ( arr , value , start=0){
     }
     return -1;
 }
-function lastIndexOf ( arr , value , start = arr.length =-1){
+function lastIndexOf (arr , value, start = arr.length =-1){
     if(!Array.isArray(arr)) return -1;
 
-    for(let i = start ; i<0 ; i--){
+    for(let i = start ; i>=0 ; i--){
         if(arr[i] === value){
             return i;
         }
