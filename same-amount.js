29d6804c28345988c4a38554de6c5678d54f0bd2
merramda <maryemerramdani52@gmail.com>
2026-07-03 15:48:19 +0100

    same

--- same-amount.js
+++ same-amount.js
@@ -1,6 +1,9 @@
 function sameAmount(str,reg1 ,reg2){
-    const matches1 = str.match(reg1);
-    const matches2 = str.match(reg2);
+    const matches1 = str.match(reg1.source, 'g');
+    const matches2 = str.match(reg2.source, 'g');
+
+    const matches1 = str.match(r1);
+    const matches2 = str.match(r2);
 
     const count1 = matches1 ?  matches1.length : 0;
     const count2 = matches2 ?  matches2.length : 0;
