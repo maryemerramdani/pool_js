e40e0cbc8823d485ad386369e504a17baaf5bbd9
merramda <maryemerramdani52@gmail.com>
2026-07-03 15:51:28 +0100

    same

--- same-amount.js
+++ same-amount.js
@@ -1,6 +1,6 @@
-function sameAmount(str,reg1 ,reg2){
-    const matches1 = str.match(reg1.source, 'g');
-    const matches2 = str.match(reg2.source, 'g');
+function sameAmount(str , reg1, reg2){
+    const r1 = new RegExp(reg1.source, 'g');
+    const r2 = new RegExp(reg2.source, 'g');
 
     const matches1 = str.match(r1);
     const matches2 = str.match(r2);
