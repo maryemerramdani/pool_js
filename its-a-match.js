73a98fcb977727e62d7215f92106d77117a7d0a3
merramda <maryemerramdani52@gmail.com>
2026-07-03 11:53:14 +0100

    match

--- its-a-match.js
+++ its-a-match.js
@@ -1,9 +1,4 @@
-const a =/ hi /;
+const a =/hi/;
 const b =/^hi/;
 const c=/hi$/;
 const d =/^hi$/;
-console.log(normal.test("oh hi there")); // true
-console.log(begin.test("hi there")); // true
-console.log(end.test("say hi")); // true
-console.log(beginEnd.test("hi")); // true
-console.log(beginEnd.test("hiya")); // false
